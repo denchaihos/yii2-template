e4e572a1a9fd520e7ee0c660442235e637e72293
den <denchaihos@gmail.com>
2017-06-26 09:21:34 +0700

    Merge origin/master
    Conflicts:
    	README.md

---
+++ frontend/themes/bootstrapmaterial/BootstrapMaterialAsset.php
@@ -0,0 +1,22 @@
+<?php
+namespace frontend\themes\bootstrapmaterial;
+
+use yii\web\AssetBundle;
+
+class BootstrapMaterialAsset extends AssetBundle{
+    public $sourcePath = '@frontend/themes/bootstrapmaterial/assets';
+    
+    public $css = [
+        'css/bootstrap-material-design.min.css',
+        'css/ripples.min.css',
+		'css/style.css',
+    ];
+    public $js = [
+        'js/material.min.js',
+        'js/ripples.min.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        //'yii\bootstrap\BootstrapAsset',
+    ];
+}
